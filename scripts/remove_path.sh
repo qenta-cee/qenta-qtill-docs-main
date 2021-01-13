4b9275b9e224be9870f2b8a63ab1b0ab80ef58b2
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-01-13 13:41:13 +0100

    Reduce antora default path to root

---
+++ scripts/remove_path.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ------------------------------------------------
+# ANTORA rewrite pges for single module in / root
+# ------------------------------------------------
+
+set -e
+
+TEMPDIR="$(mktemp -d)"
+PATH_TO_REMOVE="${1:-online-guides/main}"
+SED_BINARY='sed'
+[[ "${OSTYPE}" == 'darwin'* ]] && SED_BINARY='gsed'
+mv build/site/${PATH_TO_REMOVE}/* build/site/
+rm -r build/site/${PATH_TO_REMOVE}/
+find build/site/ -type f -name '*.html' > $TEMPDIR/files.list
+for file in $(cat $TEMPDIR/files.list); do
+  (
+  ${SED_BINARY} -i 's,\(../\)\+_/\.\./,/,' "$file"
+  ${SED_BINARY} -i 's,\(../\)\+_,/_,' "$file"
+  )&
+done
+${SED_BINARY} -i 's,'${PATH_TO_REMOVE}',,g' build/site/search-index.js
+wait
