fd1916db72f027775a110db718558a8ab3ba20e4
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-03-31 13:13:26 +0200

    CI: Add xref validity check (#1)
    * Add check for invalid references
    
    * Warn but do not abort if invalid refs found

---
+++ scripts/invalid_refs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Quick hack for getting proper paths of wrong refs
+# Author: herbert.knapp@qenta.com
+
+INVALID_REFS=$(
+  for L in "$(grep --include=\*.html -r build/site -e 'class="page unresolved"')"; do
+    ADOC=$(echo "${L}" | cut -d : -f1 | sed -e 's,build/site/online-guides/main/,content/online-guides/modules/ROOT/pages/,' | sed -e 's,/index.html,.adoc,')
+    REF=$(echo "${L}" | sed 's,.* href="#\(.*\.adoc\).*,\1,')
+    grep -n "${REF}" ${ADOC} | sed 's,^content/,,'
+  done | sort | uniq
+)
+N_REFS=$(echo "${INVALID_REFS}" | wc -l | xargs)
+N_FILES=$(echo "${INVALID_REFS}" | cut -d ':' -f1 | uniq | wc -l | xargs)
+
+[[ N_REFS == 0 ]] && exit 0
+
+echo "::warning::${N_FILES} pages contain invalid references. Please check build log for details!"
+echo "::group::Click to show files"
+echo "${INVALID_REFS}"
+echo "::endgroup::"
+
+exit 1
