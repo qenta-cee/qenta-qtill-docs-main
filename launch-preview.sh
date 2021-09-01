6b4f1ebd6fed45ea3cc2abe019967923337867e2
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-09-01 09:15:42 +0200

    Add Preview launcher

---
+++ launch-preview.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Remove old pages"
+rm -rf build/site
+
+echo "Building UI"
+make ui.build &>/dev/null
+
+echo "Building Documentation"
+make antora.build &>/dev/null
+
+make antora.run &>/dev/null &
+echo -n "Staring webserver in the background"
+while ! curl -s --fail http://localhost:8051/ >/dev/null; do echo -n .; sleep 3; done; echo
+echo
+echo "http://localhost:8051/online-guides/main/"
+echo
+echo "Watching for changes..."
+echo 
+make antora.watch >/dev/null 
+
