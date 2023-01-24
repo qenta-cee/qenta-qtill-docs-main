31c19709c3c7e680d63f9068088638ca5d11d0b8
Melisa09 <melisa.melka@hotmail.com>
2023-01-24 13:04:36 +0100

    changed port in launch preview

--- launch-preview.sh
+++ launch-preview.sh
@@ -11,11 +11,11 @@ make antora.build
 
 make antora.run &>/dev/null &
 echo -n "Staring webserver in the background"
-while ! curl -s --fail http://localhost:8051/ >/dev/null; do echo -n .; sleep 3; done; echo
+while ! curl -s --fail http://localhost:8052/ >/dev/null; do echo -n .; sleep 3; done; echo
 echo
 echo "http://localhost:8052/online-guides/main/"
 echo
 echo "Watching for changes..."
-echo 
+echo  
 make antora.watch >/dev/null 
 
